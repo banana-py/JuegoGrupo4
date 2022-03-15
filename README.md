cf9948fb2e72cb92d8e9b03e26add87f22feee77
banana-py <66887786+banana-py@users.noreply.github.com>
2022-03-15 00:02:16 -0300

    Update and rename README.md to cambio_esteban.c

--- README.md
+++
@@ -1,1 +0,0 @@
-# JuegoGrupo4
---
+++ cambio_esteban.c
@@ -0,0 +1,7 @@
+# JuegoGrupo4
+void victoria(){
+  // Procedimiento que es llamado cuando se gana el juego. Imprime un mensaje de felicitaciones.
+  printf("Felicidades!!!\n\n");
+  printf("Haz ganado el juego, no ganaste nada, solo te mantienes con vida.\n");
+  printf("Para más información escribir a @grupo4");
+}
