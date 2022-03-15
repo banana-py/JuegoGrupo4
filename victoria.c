e0b5f1f450ab16a2f5b1116acd2d396e1c7b8835
banana-py <66887786+banana-py@users.noreply.github.com>
2022-03-15 00:03:19 -0300

    Create victoria.c

---
+++ victoria.c
@@ -0,0 +1,6 @@
+void victoria(){
+  // Procedimiento que es llamado cuando se gana el juego. Imprime un mensaje de felicitaciones.
+  printf("Felicidades!!!\n\n");
+  printf("Haz ganado el juego, no ganaste nada, solo te mantienes con vida.\n");
+  printf("Para más información escribir a @grupo4");
+}
